d0dcaadc36f8011ba952dca52b062dbd2b9b4a75
Kushagra Rawat <kushagra.rawat@addverb.com>
2022-01-04 22:50:32 +0530

    added standard time format

--- CppND-System-Monitor/src/format.cpp
+++ CppND-System-Monitor/src/format.cpp
@@ -8,4 +8,9 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    int hr = (int)(seconds/3600);
+    int min = ((int)(seconds/60))%60;
+    int sec = (int)(seconds%60);
+    string time = std::to_string(hr) + ':' + std::to_string(min) + ':' + std::to_string(sec);
+    return time; }
